13158b51a4243d0b916a54863f75b765e7b4a043
ashkanmsz <ashkanmsz7878@gmail.com>
2020-07-20 19:51:34 +0430

    Fifth commit

--- src/main/java/com/ashkanmsz/docker_jenkins_integration_sample/Test1.java
+++ src/main/java/com/ashkanmsz/docker_jenkins_integration_sample/Test1.java
@@ -2,6 +2,6 @@ package com.ashkanmsz.docker_jenkins_integration_sample;
 
 public class Test1 {
 public static void main(String[] args) {
-	System.out.println("Hi");
+	System.out.println("Hello World");
 }
 }
